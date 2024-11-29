8a4eec30ea7372b4238a78e8d3ada29fa380152b
Aldair <aldairgome97@gmail.com>
2024-11-29 11:39:58 -0600

    Se añadieron las dependencias para las variables de entorno, asi mismo se añadieron las variables al proyecto.
    Se añadió el class transformer y el class validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+
+  logger.log(`Server running on port ${envs.port}`);
 }
 bootstrap();
