46664a443f13e613865fe83d0d0726b6fb495248
Aldair <aldairgome97@gmail.com>
2024-12-01 19:03:25 -0600

    Creando el auth y el users Service

---
+++ src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUser(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+}
