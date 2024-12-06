81b93cac2904a3ca3139ece7341c55dba4b3831f
Aldair <aldairgome97@gmail.com>
2024-12-05 21:14:09 -0600

    Se creó el método de login que regresa un jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { envs } from 'src/config';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: envs.jwt.secret,
+      signOptions: { expiresIn: envs.jwt.expiration },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
