90e0496a32336b7118190704c547010388316094
Aldair <aldairgome97@gmail.com>
2024-12-06 20:00:13 -0600

    Organizando los archivos de estrategias y guardias de validación de usuarios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { Public } from './decorators/public-access.decorator';
 
 @Controller()
