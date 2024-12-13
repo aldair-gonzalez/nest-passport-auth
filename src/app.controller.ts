6de12a2485e5f80c0f9e53eac98327140e6a93de
Aldair <aldairgome97@gmail.com>
2024-12-12 21:00:05 -0600

    Se movieron las rutas auth a la carpeta auth. Todas las rutas son publicas, a no ser que se les asigne un Guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,35 +1,9 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { Public } from './decorators/public-access.decorator';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
-
-  // Esta ruta es pública
-  @Public()
   @Get()
   getHello(): string {
     return 'Hello World!';
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req: any) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/logout')
-  async logout(@Request() req: any) {
-    return req.logout();
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('auth/profile')
-  async getProfile(@Request() req: any) {
-    return req.user;
   }
 }
