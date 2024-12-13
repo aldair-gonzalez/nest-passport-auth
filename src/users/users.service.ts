83f8520fa9121247400e0ddb559cd50c87c0b8ca
Aldair <aldairgome97@gmail.com>
2024-12-12 21:04:58 -0600

    Conectamos el login y el register con la base de datos

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,34 +1,26 @@
 import { Injectable } from '@nestjs/common';
-// import { Prisma, User } from '@prisma/client';
-// import { PrismaService } from 'src/prisma.service';
-
-export type User = any;
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hashPass } from 'src/utils/hashing';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(private prisma: PrismaService) {}
 
-  // constructor(private prisma: PrismaService) {}
-  // async getUser(
-  //   userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-  // ): Promise<User | null> {
-  //   return await this.prisma.user.findUnique({
-  //     where: userWhereUniqueInput,
-  //   });
-  // }
+  async findOneByEmail(email: string): Promise<User | undefined> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  async createUser(data: CreateUserDto): Promise<User> {
+    data.password = await hashPass(data.password);
+    return await this.prisma.user.create({
+      data,
+    });
   }
 }
