d51648aab75fa83cbbd2ba0808c8dbdacd1988ff
Aldair <aldairgome97@gmail.com>
2024-12-12 21:02:14 -0600

    Se pueden registrar usuarios, y se valida que la contraseña hasheada del usuario coincida con la pasada por el body de la petición

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { comparePass } from 'src/utils/hashing';
 
 @Injectable()
 export class AuthService {
@@ -9,19 +12,25 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
-      return result;
-    }
-    return null;
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(email);
+    if (!user) return null;
+
+    const isMatch = comparePass(pass, user.password);
+    if (!isMatch) return null;
+
+    const { password, ...result } = user;
+    return result;
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { email: user.email, sub: user.user_id };
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async register(user: CreateUserDto) {
+    await this.usersService.createUser(user);
   }
 }
