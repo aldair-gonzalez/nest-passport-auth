5325abfb5e2f7af35933010ee5f32cb9a95bfaac
Aldair <aldairgome97@gmail.com>
2024-12-16 19:31:47 -0600

    Eliminando commits

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-// import { APP_GUARD } from '@nestjs/core';
-// import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    // El guard de auth se usa de forma global
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: JwtAuthGuard,
-    // },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
